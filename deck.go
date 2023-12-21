40d7510978f6b8ea89b779b3d034dba19aeb8ac6
Debajit Majumder <debajit.business@gmail.com>
2023-12-21 22:02:37 +0530

    Created a newDeck method

--- deck.go
+++ deck.go
@@ -7,6 +7,20 @@ import "fmt"
 // This is a custom type
 type deck []string
 
+// Create a new function that returns a deck
+func newDec() deck {
+	cards := deck{}
+	cardSuites := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suite := range cardSuites {
+		for _, value := range cardValues {
+			cards = append(cards, value + " of " + suite)
+		}
+	}
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println( i, card )
--- main.go
+++ main.go
@@ -1,15 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
-	card := newCard()
-	fmt.Println(card)
-
-	// Slice - Defining a slice of type string
-	cardList := deck{"Ace of Diamonds", newCard()}
-	// Appending a new element to the slice
-	cardList = append(cardList, "Six of Spades")
+	// Create a new deck
+	cardList := newDec()
 
 	// Iterate over the slice
 	cardList.print()
