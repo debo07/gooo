d68aa2a9828ded7eb53c8e64fac8dab505bbf3b4
Debajit Majumder <debajit.business@gmail.com>
2023-12-26 23:08:47 +0530

    Added some tests for newDeck func

--- deck_test.go
+++ deck_test.go
@@ -2,10 +2,18 @@ package main
 
 import "testing"
 
-func TestNewDec(t *testing.T) {
+func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 16 {
 		t.Errorf("Expected the deck length of 16, but got %v", len(d))
+	}
+
+	// Add checks for 1st and last card
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected 'Ace of Spades', but received %v", d[0])
+	}
+	if d[len(d) - 1] != "Four of Clubs" {
+		t.Errorf("Expected 'Four of Clubs', but received %v", d[len(d) - 1])
 	}
 }
